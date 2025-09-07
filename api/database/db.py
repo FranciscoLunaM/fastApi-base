02a1a1446543a7fa66caba80ab5b2ae265de6f58
francisco luna <franciscoe.martinez@gmail.com>
2025-09-07 11:10:50 -0600

    added also the dotenv for EnV and changed in the db file

--- api/database/db.py
+++ api/database/db.py
@@ -1,11 +1,19 @@
 import os
 from sqlmodel import create_engine,Session
+from dotenv import load_dotenv
+load_dotenv('../.env')
+
+username=os.getenv("DATABASE_USERNAME")
+password=os.getenv("DATABASE_PASSWORD")
+host=os.getenv("DATABASE_HOST")
+port=os.getenv("DATABASE_PORT")
+database=os.getenv("DATABASE_NAME")
 
 #engine=create_engine("postgresql://username:password@ip:port/dbname", echo=True, connect_args=connect_args)
 
 
 
-engine = create_engine("postgresql://root:root@localhost:5432/pruebas", echo=True)
+engine = create_engine(f"postgresql://{username}:{password}@{host}:{port}/{database}", echo=True)
 print("Database connection established.")
 def get_session():
     with Session(engine) as session:
